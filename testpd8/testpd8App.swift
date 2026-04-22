c9b1b98cd2f70ccb8aabd0c23b1617475f9b9dc9
BT_C09_33 <mobileappdev@bths.edu>
2026-04-22 13:22:19 -0400

    Initial Commit

---
+++ testpd8/testpd8App.swift
@@ -0,0 +1,17 @@
+//
+//  testpd8App.swift
+//  testpd8
+//
+//  Created by Teacher on 4/22/26.
+//
+
+import SwiftUI
+
+@main
+struct testpd8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
